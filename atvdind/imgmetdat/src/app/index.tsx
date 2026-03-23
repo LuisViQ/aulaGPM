916a0442132dec30843537890e4a0b3df70e6317
Luis Felipe Viana Queiroz <luisqueiroz@Luiss-MacBook-Air.local>
2026-03-23 17:24:50 -0300

    removing import mapbox

--- atvdind/imgmetdat/src/app/index.tsx
+++ atvdind/imgmetdat/src/app/index.tsx
@@ -1,4 +1,4 @@
-﻿import Mapbox from "@rnmapbox/maps";
+﻿
 import { Routes } from "../routes";
 import * as SplashScreen from "expo-splash-screen";
 
@@ -6,10 +6,6 @@ SplashScreen.setOptions({
   duration: 2000,
   fade: true,
 });
-const MAPBOX_ACCESS_TOKEN = process.env.EXPO_PUBLIC_MAPBOX_ACCESS_TOKEN;
-if (MAPBOX_ACCESS_TOKEN) {
-  Mapbox.setAccessToken(MAPBOX_ACCESS_TOKEN);
-}
 
 export default function App() {
   return <Routes />;
